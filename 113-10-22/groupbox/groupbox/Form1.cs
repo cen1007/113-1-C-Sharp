6f41470289ce94d6837a69c2f00fc84f6d738d43
cen1007 <A113221006@mail.shu.edu.tw>
2024-10-22 17:59:31 +0800

    113-10-22

---
+++ 113-10-22/groupbox/groupbox/Form1.cs
@@ -0,0 +1,20 @@
+namespace groupbox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
